0300f033f1f36c4492917f849b8ee9a0eb908c9f
Weber588 <idonthavetotellyoumyname@gmail.com>
2022-08-21 10:38:10 +1000

    Refactored placeholder manager

--- src/main/java/wbs/arena/PlaceholderManager.java
+++ src/main/java/wbs/arena/PlaceholderManager.java
@@ -1,6 +1,7 @@
 package wbs.arena;
 
 import org.bukkit.OfflinePlayer;
+import wbs.arena.data.ArenaDB;
 import wbs.arena.data.ArenaPlayer;
 import wbs.utils.util.WbsEnums;
 import wbs.utils.util.pluginhooks.PlaceholderAPIWrapper;
@@ -23,51 +24,84 @@ public final class PlaceholderManager {
     }
 
     private static String parseParams(OfflinePlayer player, String params) {
-        if (params.startsWith(TOP_KEY + "_")) {
-            String[] args = params.split("_");
+        String[] args = params.split("_");
 
-            if (args.length < 3) {
-                return null;
-            }
+        String result = parseTop(player, args);
 
-            String statName = args[1];
+        if (result != null) return result;
 
-            StatsManager.TrackedStat stat = WbsEnums.getEnumFromString(StatsManager.TrackedStat.class, statName);
+        result = parseMisc(player, args);
 
-            if (stat == null) {
-                return "[Invalid stat: " + args[1] + "]";
-            }
-
-            int place;
-            try {
-                place = Integer.parseInt(args[2]);
-            } catch (NumberFormatException e) {
-                return "[Invalid place number: " + args[2] + "]";
-            }
-
-            if (place <= 0) {
-                return "[Invalid place number: " + args[2] + "]";
-            }
-
-            PlayerProperty property;
-            if (args.length >= 4) {
-                property = WbsEnums.getEnumFromString(PlayerProperty.class, args[3]);
-                if (property == null) {
-                    return "[Invalid property: " + args[3] + "]";
-                }
-            } else {
-                property = PlayerProperty.NAME;
-            }
-
-            List<ArenaPlayer> top = StatsManager.getTopCached(stat);
-            if (top.size() < place) {
-                return NOT_FOUND;
-            }
-
-            return property.getProperty(top.get(place - 1));
-        }
+        if (result != null) return result;
 
         return null;
+    }
+
+    private static String parseMisc(OfflinePlayer player, String[] args) {
+        ArenaPlayer arenaPlayer = null;
+
+        for (ArenaPlayer check : ArenaDB.getPlayerManager().getCache().values()) {
+            if (check.getName().equalsIgnoreCase(player.getName())) {
+                arenaPlayer = check;
+                break;
+            }
+        }
+
+        if (arenaPlayer == null) return null;
+
+        String prop = String.join("", args).toLowerCase();
+
+        PlayerProperty property = WbsEnums.getEnumFromString(PlayerProperty.class, prop);
+
+        if (property == null) return null;
+
+        return property.getProperty(arenaPlayer);
+    }
+
+    private static String parseTop(OfflinePlayer player, String[] args) {
+        if (!args[0].equalsIgnoreCase(TOP_KEY)) {
+            return null;
+        }
+
+        if (args.length < 3) {
+            return null;
+        }
+
+        String statName = args[1];
+
+        StatsManager.TrackedStat stat = WbsEnums.getEnumFromString(StatsManager.TrackedStat.class, statName);
+
+        if (stat == null) {
+            return "[Invalid stat: " + args[1] + "]";
+        }
+
+        int place;
+        try {
+            place = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            return "[Invalid place number: " + args[2] + "]";
+        }
+
+        if (place <= 0) {
+            return "[Invalid place number: " + args[2] + "]";
+        }
+
+        PlayerProperty property;
+        if (args.length >= 4) {
+            property = WbsEnums.getEnumFromString(PlayerProperty.class, args[3]);
+            if (property == null) {
+                return "[Invalid property: " + args[3] + "]";
+            }
+        } else {
+            property = PlayerProperty.NAME;
+        }
+
+        List<ArenaPlayer> top = StatsManager.getTopCached(stat);
+        if (top.size() < place) {
+            return NOT_FOUND;
+        }
+
+        return property.getProperty(top.get(place - 1));
     }
 
     private enum PlayerProperty {
@@ -78,6 +112,7 @@ public final class PlaceholderManager {
         POINTS,
         KILLSTREAK,
         MAXKILLSTREAK,
+        KIT,
         ;
 
         public String getProperty(ArenaPlayer player) {
@@ -89,6 +124,7 @@ public final class PlaceholderManager {
                 case POINTS -> player.getPoints() + "";
                 case KILLSTREAK -> player.getCurrentKillstreak() + "";
                 case MAXKILLSTREAK -> player.getHighestKillstreak() + "";
+                case KIT -> player.getCurrentKit().getName();
             };
         }
     }
